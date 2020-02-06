4b6751128fc7e8c8cb37485d431dca7925855771
lordgravier <baskette11@gmail.com>
2020-02-06 11:07:07 +0100

    pilote controller

--- controllers/PiloteController.js
+++ controllers/PiloteController.js
@@ -1,8 +1,14 @@
-
+let model = require('../models/pilote.js');
 // ///////////////////////// R E P E R T O I R E    D E S    P I L O T E S
 
 module.exports.Repertoire = 	function(request, response){
-   response.title = 'Répertoire des pilotes';
-
-      response.render('repertoirePilotes', response);
-  } ;
+    response.title = 'Répertoire des pilotes';
+    model.getListeInitialPilote(function(err, result){
+        if(err){
+            console.log(err);
+            return;
+        }
+        response.listeLettre = result;
+        response.render('listeLettre', response);
+    });
+} ;
