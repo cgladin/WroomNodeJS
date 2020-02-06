f8df42538bc262863edcdceb85bd226c5a7e6419
lordgravier <baskette11@gmail.com>
2020-02-06 11:00:53 +0100

    ajout fichier pilote

---
+++ models/pilote.js
@@ -0,0 +1,21 @@
+let db = require('../configDb');
+
+module.exports.getListeInitialPilote = function (callback) {
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT DISTINCT(LEFT(pilnom), 1) AS initial FROM pilote"
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
+
+module.exports.getPilote= function (initial, callback){
+
+};
