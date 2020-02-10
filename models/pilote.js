28fed03321d067a93df22d891a0df97c4e591808
Antoine <antoine.nony@etu.unilim.fr>
2020-02-10 11:04:13 +0100

    requete terminée

--- models/pilote.js
+++ models/pilote.js
@@ -17,7 +17,17 @@ module.exports.getListeInitialPilote = function (callback) {
 };
 
 module.exports.getNomImagePilote= function (initial, callback){
-    SELECT DISTINCT SUBSTR(PILNOM, 1, 1) as initiale, PILNOM, PILPRENOM, p.PILNUM, PHOADRESSE FROM PILOTE p JOIN PHOTO ph ON p.PILNUM = ph.PILNUM WHERE PHOSUJET = "Photo identité" HAVING initial = ':initial'
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT DISTINCT SUBSTR(PILNOM, 1, 1) as initiale, PILNOM, PILPRENOM, p.PILNUM, PHOADRESSE FROM PILOTE p JOIN PHOTO ph ON p.PILNUM = ph.PILNUM WHERE PHOSUJET = 'Photo identité' HAVING initial = '" + initial + "'";
+            //console.log (sql);
+            connexion.query(sql, callback);
 
-
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
 };
