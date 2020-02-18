cc89264ca030c970d25d66114f4a5fd918feab06
lordgravier <baskette11@gmail.com>
2020-02-18 14:50:04 +0100

    parrallel

--- controllers/PiloteController.js
+++ controllers/PiloteController.js
@@ -3,37 +3,32 @@ let async=require("async");
 // ///////////////////////// R E P E R T O I R E    D E S    P I L O T E S
 
 module.exports.Repertoire = function (request, response) {
-    let initial=request.params.lettre;
     response.title = 'Répertoire des pilotes';
-    async.parallel([
-        function(callback) {
-            model.getListeInitialPilote(function (err, result) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                response.listeLettre = result;
-                response.render('repertoirePilotes', response);
-            });
+    model.getListeInitialPilote(function (err, result) {
+        if (err) {
+            console.log(err);
+            return;
         }
-    ],
-        function(callback){
-            model.getNomImagePilote(initial,)
-        }
-);
+        response.listeLettre = result;
+        response.render('repertoirePilotes', response);
+    });
 };
 
-// //////////////////////// D E T A I L    D E S    P I L O T E S
 module.exports.NomPilote = function (request, response) {
-    let data = request.params.inital;
+    let data = request.params.initial;
     response.title = 'Pilote dont le nom commence par ' + data;
     async.parallel([
             function (callback) {
-                model.getListeInitialPilote(data, (function (errPil, resultPil) {
+                model.getListeInitialPilote( function (errPil, resultPil) {
                     callback(null, resultPil)
-                }));
+                });
                 // pour afficher à nouveau les premières lettres des pilotes
             }, // fin callback1
+            function (callback) {
+                model.getNomImagePilote(data,function (err, result) {
+                    callback(null,result)
+                })
+            }
         ],
         function (err, result) {
             if (err) {
@@ -41,9 +36,9 @@ module.exports.NomPilote = function (request, response) {
                 console.log(err);
                 return;
             }
-            response.listePilo = result[0]; // liste des premières lettres
+            response.listeLettre = result[0]; // liste des premières lettres
             response.data = result[1]; // les pilotes dont le nom
-            response.render('listePiloteNom', response);
+            response.render('resultatRepertoirePilotes', response);
         }  // fin fonction
     );  // fin async
 };  // fin module
