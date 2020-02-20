0e807c7b33e710126149d58b145e5fb628bf9545
Antoine <antoine.nony@etu.unilim.fr>
2020-02-20 10:53:38 +0100

    ajout requêtes

--- models/pilote.js
+++ models/pilote.js
@@ -31,3 +31,53 @@ module.exports.getNomImagePilote= function (initial, callback){
         }
     });
 };
+
+module.exports.getDetailPilotePerso= function (pilnum, callback){
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT PILNOM, PILPRENOM, PILDATENAIS, PILPOIDS, PILTAILLE, PILTEXTE, ECUNOM, PAYNAT FROM PILOTE p JOIN PAYS pa ON p.PAYNUM = pa.PAYNUM JOIN ECURIE e ON p.ECUNUM = e.ECUNUM WHERE PILNUM=" + pilnum + "";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
+
+module.exports.getDetailPiloteSponsor= function (pilnum, callback){
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT SPONOM, SPOSECTACTIVITE, FROM PILOTE p JOIN SPONSORISE s ON p.PILNUM = s.PILNUM JOIN SPONSOR sp ON s.SPONUM = sp.SPONUM WHERE PILNUM= " + pilnum + "";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
+
+module.exports.getDetailPilotePhotos= function (pilnum, callback){
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT PHOSUJET, PHOCOMMENTAIRE, PHOADRESSE FROM PILOTE p JOIN PHOTO ph ON p.PILNUM = ph.PILNUM WHERE PHONUM !=1 AND PILNUM=" + pilnum + "";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
+
+
