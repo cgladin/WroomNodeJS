dfb5c209a06bf6998e6adfa64cc6037b80ce639e
lordgravier <baskette11@gmail.com>
2020-02-24 15:59:56 +0100

    Detail circuit fonctionnel

--- controllers/CircuitController.js
+++ controllers/CircuitController.js
@@ -1,4 +1,5 @@
 let model = require('../models/circuit.js');
+let async=require("async");
 // ////////////////////// L I S T E R     C I R C U I T S
 module.exports.ListerCircuit = function(request, response){
     response.title = 'Liste des circuits';
@@ -10,4 +11,30 @@ module.exports.ListerCircuit = function(request, response){
         response.listeCircuit = result;
         response.render('listerCircuit', response);
     });
-}
+};
+module.exports.DetailCircuit=function (request, response) {
+let num = request.params.CIRNUM;
+    async.parallel([
+    function (callback) {
+        model.getListeCircuit(function (err,result) {
+            callback(null,result)
+        })
+    },
+        function (callback) {
+            model.getDetailCircuit(num,function (err,result) {
+                callback(null,result)
+            })
+        }
+    ],
+        function (err, result) {
+            if (err) {
+                // gestion de l'erreur
+                console.log(err);
+                return;
+            }
+            response.listeCircuit=result[0];
+            response.detailCircuit=result[1][0];
+            response.render('detailCircuit', response);
+        }
+    );
+};
--- models/circuit.js
+++ models/circuit.js
@@ -15,3 +15,19 @@ module.exports.getListeCircuit= function (callback){
         }
     });
 };
+
+module.exports.getDetailCircuit= function (cirnum, callback){
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT CIRNOM, CIRLONGUEUR, CIRNBSPECTATEURS, CIRADRESSEIMAGE, CIRTEXT, PAYNOM FROM circuit c JOIN pays p ON c.PAYNUM=p.PAYNUM WHERE CIRNUM="+cirnum+"" ;
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
