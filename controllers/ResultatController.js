7e661e24f1485638de86081507d399b3a5a6457d
lordgravier <baskette11@gmail.com>
2020-03-09 08:07:57 +0100

    po fini

--- controllers/ResultatController.js
+++ controllers/ResultatController.js
@@ -1,7 +1,47 @@
+let model = require('../models/grandprix.js');
+let courseModel = require('../models/course.js');
+let async=require("async");
   // //////////////////////////L I S T E R    R E S U L T A T S
 module.exports.ListerResultat = function(request, response){
-
 	response.title = 'Liste des résulats des grands prix';
-
-	response.render('listerResultat', response);
+	model.getListeGrandPrix(function (err, result) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		response.listeGrandPrix = result;
+		response.render('listerResultat', response);
+	});
+};
+module.exports.DetailGrandPrix= function (request, response) {
+	let gpnum = request.params.GPNUM;
+	async.parallel([
+			function (callback) {
+				model.getListeGrandPrix( function (err, result) {
+					callback(null, result)
+				});
+			},
+			function (callback) {
+				model.getNomGrandPrix(gpnum,function (err, result) {
+					callback(null,result)
+				})
+			},
+			function(callback){
+				courseModel.getTempsCourse(gpnum, function (err,result) {
+					callback(null,result)
+				})
+			}
+		],
+		function (err, result) {
+			if (err) {
+				// gestion de l'erreur
+				console.log(err);
+				return;
+			}
+			response.listeGrandPrix = result[0];
+			response.nomGrandPrix = result[1];
+			response.listeResultat= result[2];
+			response.render('detailGrandPrix', response);
+		}  // fin fonction
+	);  // fin async
 };
