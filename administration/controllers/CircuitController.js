955404add0d8a917bf865ef614e88eb4d61f312a
lordgravier <baskette11@gmail.com>
2020-04-08 18:10:35 +0200

    ajout circuti html

--- administration/controllers/CircuitController.js
+++ administration/controllers/CircuitController.js
@@ -1,6 +1,15 @@
+var async = require('async');
+let model = require('../models/circuit.js');
 // ////////////////////// L I S T E R     C I R C U I T S
 
 module.exports.ListerCircuit = function(request, response){
     response.title = 'Liste des circuits';
-    response.render('listerCircuit', response);
+    model.getCircuits(function(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        response.circuits = result;
+        response.render('circuits/gestionCircuits', response);
+    });
 }
---
+++ administration/models/circuit.js
@@ -0,0 +1,17 @@
+let db = require('../configDb');
+
+module.exports.getCircuits= function (callback) {
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT CIRNUM, CIRNOM, CIRLONGUEUR,CIRNBSPECTATEURS FROM circuit GROUP BY CIRNOM ASC";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
