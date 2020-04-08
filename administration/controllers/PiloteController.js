0f292945ff974081d511922d3351abbbad69ff16
lordgravier <baskette11@gmail.com>
2020-04-08 13:57:19 +0200

    selection des pilotes

--- administration/controllers/PiloteController.js
+++ administration/controllers/PiloteController.js
@@ -3,6 +3,12 @@ let model = require('../models/pilote.js');
 // ///////////////////////// R E P E R T O I R E    D E S    P I L O T E S
 
 module.exports.ListerPilote = 	function(request, response){
-
-      response.render('pilotes/gestionPilotes', response);
+    model.getListePilote(function (err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        response.pilotes = result;
+        response.render('pilotes/gestionPilotes', response);
+    });
   } ;
--- administration/models/pilote.js
+++ administration/models/pilote.js
@@ -1,1 +1,17 @@
-var db = require('../configDb');
+let db = require('../configDb');
+
+module.exports.getListePilote= function (callback) {
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT PILNOM, PILPRENOM, PILDATENAIS, PILNUM FROM pilote GROUP BY PILNOM ASC";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
